b115c3135e8ffc165b45ca445d64ccf98c32fda3
DeLit X <58394361+DeLitX@users.noreply.github.com>
2020-02-26 16:02:11 +0200

    some

---
+++ app/src/main/java/com/example/flex/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.flex;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new HomeFragment()).commit();
+        setActionListener();
+
+    }
+    private void setActionListener(){
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"it works",Toast.LENGTH_LONG).show();
+            }
+        });
+        BottomNavigationView bnv=findViewById(R.id.bottom_bar);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {//here code in case of choosing item in bottom bar
+                Fragment selectedFragment=null;
+                switch(menuItem.getItemId()){
+                    case R.id.action_account:
+                        selectedFragment=new AccountFragment();
+                        break;
+                    case R.id.action_camera:
+                        selectedFragment=new PhotoFragment();
+                        break;
+                    case R.id.action_home:
+                        selectedFragment=new HomeFragment();
+                        break;
+                    case R.id.action_map:
+                        selectedFragment=new MapFragment();
+                        break;
+                    case R.id.action_tv:
+                        selectedFragment=new TvFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,selectedFragment).commit();
+                return true;
+            }
+        });
+    }
+}
