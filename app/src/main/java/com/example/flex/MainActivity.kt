c2e8db8491183585a59ea15ad46f5295c71bdacd
DeLitX <dimkanan@gmail.com>
2020-02-29 12:35:47 +0200

    Now activities saves after changing

--- app/src/main/java/com/example/flex/MainActivity.kt
+++ app/src/main/java/com/example/flex/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    var account=AccountFragment()
+    var home=HomeFragment()
+    var tv=TvFragment()
+    var map=MapFragment()
+    var camera=CameraFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.frame_container, HomeFragment())
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, home,"fragment_tag")
             .commit()
         setActionListener()
     }
@@ -21,14 +26,29 @@ class MainActivity : AppCompatActivity() {
             //here code in case of choosing item in bottom bar
             var selectedFragment: Fragment
             when (menuItem.itemId) {
-                R.id.action_account -> selectedFragment = AccountFragment()
-                R.id.action_camera -> selectedFragment = CameraFragment()
-                R.id.action_home -> selectedFragment = HomeFragment()
-                R.id.action_map -> selectedFragment = MapFragment()
-                R.id.action_tv -> selectedFragment = TvFragment()
-                else->selectedFragment=HomeFragment()
+                R.id.action_account -> {
+                    if(supportFragmentManager.findFragmentByTag("fragment_tag")!!.equals(account))
+                    account=AccountFragment()
+                selectedFragment=account }
+                R.id.action_camera -> {
+                    if(supportFragmentManager.findFragmentByTag("fragment_tag")!!.equals(camera))
+                    camera=CameraFragment()
+                selectedFragment=camera }
+                R.id.action_home -> {
+                    if(supportFragmentManager.findFragmentByTag("fragment_tag")!!.equals(home))
+                    home=HomeFragment()
+                    selectedFragment=home }
+                R.id.action_map -> {
+                    if(supportFragmentManager.findFragmentByTag("fragment_tag")!!.equals(map))
+                    map=MapFragment()
+                    selectedFragment=map }
+                R.id.action_tv -> {
+                    if(supportFragmentManager.findFragmentByTag("fragment_tag")!!.equals(tv))
+                    tv=TvFragment()
+                    selectedFragment=tv }
+                else->selectedFragment= getSupportFragmentManager().findFragmentById(R.id.frame_container)!!
             }
-            supportFragmentManager.beginTransaction().replace(R.id.frame_container, selectedFragment).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.frame_container, selectedFragment,"fragment_tag").commit()
             true
         }
     }
