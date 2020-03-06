eb8fa6e140751f9e18dd0bc3cd5b8cb50b022b59
hortmagen21 <max_yudkin@ukr.net>
2020-03-06 21:19:45 +0200

    no message

--- accBase/urls.py
+++ accBase/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^registration', views.registration, name='registration'),
     url(r'^login', views.login, name='login'),
     url(r'^logout', views.logout, name='logout'),
-    url(r'^logout', views.getCookie(), name='getCookie'),
+    url(r'^getCookie', views.getCookie(), name='getCookie'),
 ]
