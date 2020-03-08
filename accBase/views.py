88bc3042b884209e19bf0b31cabc98629e7db01b
hortmagen21 <max_yudkin@ukr.net>
2020-03-08 20:24:24 +0200

    upg log and reg

--- accBase/views.py
+++ accBase/views.py
@@ -6,6 +6,7 @@ from .models import Users
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt,ensure_csrf_cookie
 import django
+from django.contrib.auth.decorators import login_required
 
 
 #def setSessionHash(session):
@@ -15,51 +16,69 @@ import django
 
 @csrf_exempt
 def registration(request):
-    username = request.POST.get(['username'][0], False)
-    password = request.POST.get(['password'][0], False)
-    email = request.POST.get(['email'][0], False)
+    if request.method == 'POST':
+        username = request.POST.get(['username'][0], False)
+        password = request.POST.get(['password'][0], False)
+        email = request.POST.get(['email'][0], False)
 
-    if username == False or password == False or email == False:
-        return HttpResponse("NOT VALID DATA")
+        if username == False or password == False or email == False:
+            return HttpResponse("NOT VALID DATA", status=415)
 
-    user = User.objects.create_user(username=username,password=password,email=email)
-    user.save()
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
 
     #setSessionHash(request.session)
     #session_hash = request.session.session_key
     #HttpResponse.__setitem__(header='Authorization', value=session_hash)
 
-    csrf_token = django.middleware.csrf.get_token(request)
-    http_resp=HttpResponse()
-    http_resp.__setitem__(header='X-CSRFToken', value=str(csrf_token))
-    print('I created user!!!!!!!')
+        csrf_token = django.middleware.csrf.get_token(request)
+        http_resp=HttpResponse()
+        http_resp.__setitem__(header='X-CSRFToken', value=str(csrf_token))
+        print('I created user!!!!!!!')
 
     #serialized=UserSerializer(data=request.DATA)
 
-    return HttpResponse(http_resp)
+        return HttpResponse(http_resp)
+    else:
+        return HttpResponse("Pls ensure that you use POST method", status=405)
 
 
 @csrf_exempt
 def login(request):
-    username = request.POST.get(['username'][0], False)
-    password = request.POST.get(['password'][0], False)
-    user = auth.authenticate(username=username,password=password)
+    if request.method == 'GET':
+        username = request.GET.get(['username'][0], False)
+        password = request.GET.get(['password'][0], False)
+        print(username, type(username))
+        print(password, type(password))
+        user = auth.authenticate(username=username, password=password)
 
-    if user is not None and user.is_active:
-        auth.login(request, user)
-        #setSessionHash(request.session)
-        #session_hash = request.session.session_key
-        csrf_token = django.middleware.csrf.get_token(request)
-        http_resp = HttpResponse()
-        http_resp.__setitem__(header='X-CSRFToken', value=str(csrf_token))
-        print('I log in !!!!!!!')
-        return HttpResponse('Successful login'+user.id)
-    return HttpResponse("Unsuccessful login",status=404)
+        if username == False or password == False:
+            return HttpResponse("NOT VALID DATA", status=415)
+
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            #setSessionHash(request.session)
+            #session_hash = request.session.session_key
+            csrf_token = django.middleware.csrf.get_token(request)
+            http_resp = HttpResponse()
+            http_resp.__setitem__(header='X-CSRFToken', value=str(csrf_token))
+            print('I log in !!!!!!!')
+            return HttpResponse('Successful login')
+        return HttpResponse("Unsuccessful login", status=404)
+    else:
+        return HttpResponse("Pls ensure that you use GET method", status=405)
 
 
-@csrf_exempt
 def logout(request):
-    auth.logout(request)
-    return HttpResponse('acclogout')
+    if request.method == 'GET':
+        auth.logout(request)
+        print('I logged out!!!')
+        return HttpResponse('acclogout')
+    else:
+        return HttpResponse("Pls ensure that you use GET method", status=405)
+
+
+
+
 
 
