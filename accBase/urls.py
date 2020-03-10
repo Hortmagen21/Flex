45f2da4c170eefec28976951d3be10557da13db3
hortmagen21 <max_yudkin@ukr.net>
2020-03-10 21:20:49 +0200

    send email verification

--- accBase/urls.py
+++ accBase/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^registration', views.registration, name='registration'),
+    url(r'^registration/ended', views.verifying, name='verifying'),
     url(r'^login', views.login, name='login'),
     url(r'^logout', views.logout, name='logout'),
     url(r'^forgot_pass', views.forgot_pass, name='forgot_pass'),
--- accBase/views.py
+++ accBase/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.sessions.models import Session
 from rest_framework import authtoken
@@ -14,6 +14,7 @@ from django.core.mail import send_mail
 # def setSessionHash(session):
 # session_hash = session.session_key
 # HttpResponse.__setitem__(header='Authorization', value=session_hash)
+core_url='https://sleepy-ocean-25130.herokuapp.com/'
 
 
 @csrf_exempt
@@ -27,7 +28,12 @@ def registration(request):
             return HttpResponse("NOT VALID DATA", status=415)
 
         user = User.objects.create_user(username=username, password=password, email=email)
+        user.is_active = False
         user.save()
+
+        url_confirm=core_url+'registration/ended?email={}'.format(user.email)
+        send_mail('Verify Flex account', 'End up your registration by this url {}'.format(url_confirm),
+                  'hortmagennn@gmail.com', email, fail_silently=False)
 
     # setSessionHash(request.session)
     # session_hash = request.session.session_key
@@ -81,25 +87,40 @@ def logout(request):
         return HttpResponse("Pls ensure that you use GET method", status=405)
 
 
-def forgot_pass(request):
-    token = PasswordGenerator(minlen=8, maxlen=8)
-    user_email = ''
+def verifying(request):
     if request.method == 'GET':
-        email = request.GET.get(['email'][0], False)
-        user_email=email
-        if email:
-            send_mail('Change Flex Password!', 'The SECRETE code number is {}'.format(token), 'hortmagennn@gmail.com',
-                      email, fail_silently=False)
-            HttpResponse('Message is sent')
-        else:
-            HttpResponse('Bad email', status=404)
-    if request.method == 'POST':
-        user_token = request.POST.get(['user_token'][0], False)
-        new_password=request.POST.get(['new_password'][0], False)
-        if user_token and user_token == token:
-            user = User.objects.get(email=user_email)
-            user.set_password(new_password)
-            user.save()
+        user_email=request.GET.get('email', '')
+        user = User.objects.get(email=user_email)
+        user.is_active = True
+        user.save()
+        return HttpResponseRedirect('flex://www.main.com/end_registration')
+    else:
+        return HttpResponse("Pls ensure that you use GET method", status=405)
+
+# user_created_token = -1
+# user_email = ''
+
+
+# def forgot_pass(request): || reset_pass
+
+    # if request.method == 'GET':
+        # token = PasswordGenerator(minlen=8, maxlen=8)
+        # user_created_token = token
+        # email = request.GET.get(['email'][0], False)
+        # user_email = email
+        # if email:
+            # send_mail('Change Flex Password!', 'The SECRETE code number is {}'.format(token), 'hortmagennn@gmail.com',
+                      # email, fail_silently=False)
+            # HttpResponse('Message is sent')
+        # else:
+            # HttpResponse('Bad email', status=404)
+    # if request.method == 'POST':
+        # user_token = request.POST.get(['user_token'][0], False)
+        # new_password=request.POST.get(['new_password'][0], False)
+        # if user_token and user_token == user_created_token:
+            # user = User.objects.get(email=user_email)
+            # user.set_password(new_password)
+            # user.save()
 
 
 
