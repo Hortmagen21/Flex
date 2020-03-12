d4950b0dfd8a2f93d5c8958bba6df7746551a52c
hortmagen21 <max_yudkin@ukr.net>
2020-03-12 16:47:30 +0200

    namespace url

--- accBase/urls.py
+++ accBase/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('registration', views.registration),
-    path('registration/ended', views.verifying),
+    path('registration/ended', views.verifying, namespace='flex'),
     path('login', views.login),
     path('logout', views.logout),
     path('forgot_pass', views.forgot_pass),
