d916590611eb4ca2bd34dfd8cc8814b7f475bfee
hortmagen21 <max_yudkin@ukr.net>
2020-03-12 14:46:37 +0200

    url transform to path

--- accBase/urls.py
+++ accBase/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 urlpatterns = [
-    url(r'^registration', views.registration, name='registration'),
-    url(r'^registration/ended', views.verifying, name='verifying'),
-    url(r'^login', views.login, name='login'),
-    url(r'^logout', views.logout, name='logout'),
-    url(r'^forgot_pass', views.forgot_pass, name='forgot_pass'),
-    url(r'^reset_pass', views.reset_pass, name='reset_pass'),
+    path('registration', views.registration),
+    path('registration/ended', views.verifying),
+    path('login', views.login),
+    path('logout', views.logout),
+    path('forgot_pass', views.forgot_pass),
+    path('reset_pass', views.reset_pass),
 
 ]
