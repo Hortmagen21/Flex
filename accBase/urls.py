2e6223b98d98a10626f06e005a3f8535b5e18678
hortmagen21 <max_yudkin@ukr.net>
2020-03-12 17:07:48 +0200

    no message

--- accBase/urls.py
+++ accBase/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.urls import path
 
 from . import views
 urlpatterns = [
     path('registration', views.registration),
-    path('registration/ended', views.verifying, namespace='flex'),
+    path('registration/ended', views.verifying),
     path('login', views.login),
     path('logout', views.logout),
     path('forgot_pass', views.forgot_pass),
     path('reset_pass', views.reset_pass),
 
 ]
+
