9794662a9e568a103ac76461dac7324471fbe694
hortmagen21 <max_yudkin@ukr.net>
2020-03-12 17:12:14 +0200

    no message

--- accBase/views.py
+++ accBase/views.py
@@ -100,7 +100,7 @@ def verifying(request):
         else:
             user.is_active = True
             user.save()
-            return HttpResponseRedirect(reverse('flex://main'))
+            return HttpResponseRedirect('flex://main')
     else:
         return HttpResponse("Pls ensure that you use GET method", status=405)
 
