2275b88e74c28ce4ecc937b30d771370c03db560
hortmagen21 <max_yudkin@ukr.net>
2020-03-16 14:21:00 +0200

    no message

--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.sessions.models import Session
+from rest_framework import authtoken
+from .models import TokenConfirm
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt,ensure_csrf_cookie,csrf_protect
+import django
+from django.contrib.auth.decorators import login_required
+from password_generator import PasswordGenerator
+from django.core.mail import send_mail
+from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+from django.template import loader
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+
