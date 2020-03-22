35ecb512f1cd0e00bc571adc2718468e5911148a
hortmagen21 <max_yudkin@ukr.net>
2020-03-22 14:13:57 +0200

    small fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__), 'tv_shows/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
