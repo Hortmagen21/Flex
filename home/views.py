29c9a1e884edc475a51c24216f76cf712fd147db
hortmagen21 <max_yudkin@ukr.net>
2020-04-03 14:36:39 +0300

    home/home

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie, csrf_protect
-from user_profile.models import UserFollower, PostBase
-from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
+from user_profile.models import UserFollower, PostBase, Likes, Comments
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 core_url = 'https://sleepy-ocean-25130.herokuapp.com/'
 test_url = 'http://127.0.0.1:8000/'
 
@@ -17,9 +18,23 @@ def view_home(request):
         id_list = []
         for user in followed:
             id_list.append(user['id'])
-        post = list(PostBase.objects.filter(user_id__in=id_list, id__gt=last_id).values('id')[:10])
-        print(post)
-        response = JsonResponse({"posts": post})
+        posts = list(PostBase.objects.filter(user_id__in=id_list, id__gt=last_id).values('id')[:10])
+
+        information = []
+        for id_of_post in posts:
+            id_post = id_of_post['id']
+            try:
+                post = PostBase.objects.get(id=int(id_post))
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound()
+            except MultipleObjectsReturned:
+                return HttpResponseBadRequest()
+            else:
+                likes = list(Likes.objects.filter(id_post=int(id_post)))
+                comments = list(Comments.objects.filter(id_post=int(id_post)))
+                information.append({'src': post.img, 'description': post.description, 'likes': len(likes), 'comments': len(comments)})
+
+        response = JsonResponse({"posts": information})
         return response
     else:
         return HttpResponse("Pls ensure that you use GET method", status=405)
