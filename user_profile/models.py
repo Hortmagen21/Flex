4dc73565a3b7ae19b564830044d4f14fa4125a0b
hortmagen21 <max_yudkin@ukr.net>
2020-05-13 12:10:31 +0300

    test db

--- user_profile/models.py
+++ user_profile/models.py
@@ -23,7 +23,7 @@ class PostBase(models.Model):
     milliseconds = models.BigIntegerField()
     img = models.CharField(max_length=100)
     description = models.CharField(max_length=100, blank=True, null=True)
-    img_mini = models.CharField(max_length=100)
+    img_mini = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         ordering = ('milliseconds',)
