f2d438ac0211a0e21fe61a9904934abf979860d2
hortmagen21 <max_yudkin@ukr.net>
2020-05-18 00:30:37 +0300

    test

--- chatroom/consumers.py
+++ chatroom/consumers.py
@@ -60,6 +60,7 @@ class ChatConsumer(AsyncConsumer):
         #print(dict_data['text'] + " PLUS " + dict_data['time'])
 
         await self.save_msg(str(dict_data['text']), int(dict_data['time']))
+        close_old_connections()
 
         await self.channel_layer.group_send(
         self.chat_room,
