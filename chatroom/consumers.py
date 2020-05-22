ca1b006f2c688bc9c7d3de0d45411baf819fe443
hortmagen21 <max_yudkin@ukr.net>
2020-05-22 19:47:24 +0300

    test cloud notification

--- chatroom/consumers.py
+++ chatroom/consumers.py
@@ -15,7 +15,11 @@ from asgiref.sync import sync_to_async
 from fcm_django.models import AbstractFCMDevice
 from fcm_django.fcm import fcm_send_message
 
-online_users = set()
+#users_id=set()
+user_to_chats={}
+
+online_users = []
+
 #SERVER_KEY=
 class ChatConsumer(AsyncConsumer):
 
@@ -28,18 +32,18 @@ class ChatConsumer(AsyncConsumer):
 
         other_user=str(self.scope['url_route']['kwargs']['username'])
         me=str(self.scope['user'])
-        print(self.scope['cookies']['id'],'COOKIE')
+        #print(self.scope['cookies']['id'],'COOKIE')
         #for i in self.scope['cookies']:
             #print(i,'test')
         #print(self.scope['_auth_user_id'],'IT IS ID USER!!!!')
-        print(self.scope['user'], 'IT IS USER!!!!')
+        #print(self.scope['user'], 'IT IS USER!!!!')
         self.me= me
         treat_obj=await self.get_tread(me,other_user)
         close_old_connections()
         self.treat_obj = int(treat_obj)
         #close_old_connections()
-        print(treat_obj,'HERE')
-        print(self.scope["headers"],'HEADDERS')
+        #print(treat_obj,'HERE')
+        #print(self.scope["headers"],'HEADDERS')
         #if treat_obj != -1:
         chat_room=f"chat_{treat_obj}"
         self.chat_room =  chat_room
@@ -50,7 +54,10 @@ class ChatConsumer(AsyncConsumer):
         })
 
         # 2 = userid
-        online_users.add({int(self.scope['cookies']['id']):self.treat_obj})
+        #users_id.add(int(self.scope['cookies']['id']))
+        user_to_chats[int(self.scope['cookies']['id'])] = int(treat_obj)
+        print(user_to_chats,'USERS_TO_CHAT')
+        #online_users.append({int(self.scope['cookies']['id']):self.treat_obj})
 
 
         await self.channel_layer.group_add(
@@ -73,28 +80,68 @@ class ChatConsumer(AsyncConsumer):
             data = {'text': dict_data['text'],
                     'time': dict_data['time'],
                     }
+
+        isOnline = False
         for user in receivers_ids:
-            close_old_connections()
             try:
-                online_users[int(user)]
+                user_to_chats[int(user)]
             except KeyError:
-                token=await self.get_user_token(int(user))
+                close_old_connections()
+                token = await self.get_user_token(int(user))
                 close_old_connections()
                 fcm_send_message(registration_id=token, data={"text":"TEST"})
             else:
-                close_old_connections()
-                await self.save_msg(self.treat_obj,str(dict_data['text']), int(dict_data['time']))
+                if user_to_chats[int(user)]==int(self.treat_obj):
+                    close_old_connections()
+                    await self.save_msg(self.treat_obj, str(dict_data['text']), int(dict_data['time']))
 
-                close_old_connections()
+                    close_old_connections()
 
-                await self.channel_layer.group_send(
-                self.chat_room,
+                    await self.channel_layer.group_send(
+                        self.chat_room,
 
-                {
-                    "type":"chat_message",
-                    "text":json.dumps(data),
+                        {
+                            "type": "chat_message",
+                            "text": json.dumps(data),
+                            # "text": front_text,
+                        })
+                else:
+                    close_old_connections()
+                    token = await self.get_user_token(int(user))
+                    close_old_connections()
+                    fcm_send_message(registration_id=token, data={"text": "TEST"})
+
+
+            #for element in online_users:
+                #try:
+                    #element[int(user)]
+                #online_users[int(user)]
+                #except KeyError:
+                    #isOnline=False
+
+                #token=await self.get_user_token(int(user))
+                #close_old_connections()
+                #fcm_send_message(registration_id=token, data={"text":"TEST"})
+                #else:
+                    #isOnline=True
+                    #break
+
+            #if isOnline:
+                #close_old_connections()
+                #await self.save_msg(self.treat_obj,str(dict_data['text']), int(dict_data['time']))
+
+                #close_old_connections()
+
+                #await self.channel_layer.group_send(
+                #self.chat_room,
+
+                #{
+                    #"type":"chat_message",
+                    #"text":json.dumps(data),
                     #"text": front_text,
-                })
+                #})
+            #else:
+
             close_old_connections()
 
 
@@ -121,7 +168,7 @@ class ChatConsumer(AsyncConsumer):
         })
 
     async def websocket_disconnect(self,event):
-        online_users.remove({2:self.treat_obj})
+        del user_to_chats[int(self.scope['cookies']['id'])]
         print('disconnected', event)
 
 
