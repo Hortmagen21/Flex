228af8205fc291edd407e48d684a23f4aeca4fe2
hortmagen21 <max_yudkin@ukr.net>
2020-05-25 10:32:49 +0300

    search people

--- tv_shows/views.py
+++ tv_shows/views.py
@@ -20,11 +20,15 @@ test_url = 'http://127.0.0.1:8000/'
 def search_people(request):
     if request.method == 'GET':
         name = request.GET.get('name', '')
-        user_row = list(User.objects.filter(username__icontains=name))
-        user_list = {}
-        for user in user_row:
-            user_list.update({user.username: user.id})
-        return JsonResponse({"user_list": user_list}, content_type='application/json')
+        user_row_start_with = list(User.objects.filter(username__istartswith=name))
+        user_row_contains = list(User.objects.filter(username__icontains=name).exclude(username__istartswith=name))
+        user_start_with_list = {}
+        user_contains_list = {}
+        for user in user_row_start_with:
+            user_start_with_list.update({user.username: user.id})
+        for user in user_row_contains:
+            user_contains_list.update({user.username: user.id})
+        return JsonResponse({"user_start_with_list": user_start_with_list, "user_contains_list": user_contains_list}, content_type='application/json')
     else:
         return HttpResponse("Pls ensure that you use GET method", status=405)
 
