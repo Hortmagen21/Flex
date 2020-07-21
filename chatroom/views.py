582baf2ed00f5b0e07ab2dbc7a93397e42bf5daa
hortmagen21 <max_yudkin@ukr.net>
2020-07-21 13:51:22 +0300

    return to psycopg in create chat but with conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)

--- chatroom/views.py
+++ chatroom/views.py
@@ -17,7 +17,7 @@ from django.core.exceptions import MultipleObjectsReturned,ObjectDoesNotExist
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseBadRequest
 from django.http import JsonResponse
 import psycopg2
-import aiopg
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 core_url = 'https://sleepy-ocean-25130.herokuapp.com/'
 test_url = 'http://127.0.0.1:8000/'
 
@@ -253,25 +253,19 @@ def create_chat_ws(receiver_name, user_name):
     else:
         receiver_id = int(receiver.id)
         user_id = int(user.id)
-        dsn = 'dbname=d7f6m0it9u59pk user=iffjnrmpbopayf password=20d31f747b4397c839a05d6d70d2decd02b23a689d86773a84d8dcfa23428946 host=ec2-54-83-1-101.compute-1.amazonaws.com'
-        pool = aiopg.create_pool(dsn)
-        conn = pool.acquire()
-        cur = conn.cursor()
-        args = (user_id, receiver_id)
-        await cur.callproc('is_chat', args)
-        chat_exist = cur.fetchall()[0][0]
-        '''conn = psycopg2.connect(dbname='d7f6m0it9u59pk', user='iffjnrmpbopayf',
+
+        conn = psycopg2.connect(dbname='d7f6m0it9u59pk', user='iffjnrmpbopayf',
                                 password='20d31f747b4397c839a05d6d70d2decd02b23a689d86773a84d8dcfa23428946',
                                 host='ec2-54-83-1-101.compute-1.amazonaws.com')
         print('CONNECTED TO BD')
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = conn.cursor()
         print('CURSOR CONNECTED')
         args = (user_id, receiver_id)
         cursor.callproc('is_chat', args)
         print('CALLPROC IS_CHAT2')
         chat_exist = cursor.fetchall()[0][0]
-        print(chat_exist,' FETCHALL2')'''
-
+        print(chat_exist,' FETCHALL2')
 
         """try:
             chat_list = list(ChatMembers.objects.filter(user_id=user_id))
@@ -299,16 +293,16 @@ def create_chat_ws(receiver_name, user_name):
 
         if chat_exist:
             args = (user_id, receiver_id)
-            await cur.callproc('chat_id', args)
+            cursor.callproc('chat_id', args)
             print('CALLPROC chat_id2')
-            chat_response = int(cur.fetchall()[0][0])
+            chat_response = int(cursor.fetchall()[0][0])
             print('fetchall2')
-            #cursor.close()
-            #conn.close()
+            cursor.close()
+            conn.close()
             print('ALL CLOSED')
         else:
-            #cursor.close()
-            #conn.close()
+            cursor.close()
+            conn.close()
             print('ALL CLOSED2')
             creating_chat = Chat(chat_admin=user_id, chat_members=2)
             creating_chat.save()
