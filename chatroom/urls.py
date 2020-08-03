4ce96ba840d22c2d178a6e2604b201cb930e65b9
hortmagen21 <max_yudkin@ukr.net>
2020-08-03 22:54:09 +0300

    activated test version of add_to_group_chat

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('upload_messages', views.upload_messages),#rewrite SQL
     path('create_group_chat', views.create_group_chat),#rewrite SQL
     path('get_chat_members', views.get_chat_members),
-    #path('add_to_group_chat', views.add_to_group_chat),
+    path('add_to_group_chat', views.add_to_group_chat),
     #path('remove_from_group_chat', views.remove_from_group_chat),
 
 
